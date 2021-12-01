ce69a349f20c99feca59b2bb8891948e01df9c54
Luke Phyall <shoreditchwarlock@gmail.com>
2021-12-01 13:22:13 +0000

    Block out NewsFeed

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { NewsHeader } from "./features/NewsHeader/NewsHeader";
 import { NewsFeed } from "./features/NewsFeed/NewsFeed";
 import { NewsFooter } from "./features/NewsFooter/NewsFooter";
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <div>
-      <NewsHeader />
-      <NewsFeed />
-      <NewsFooter />
+    <div className={styles.width}>
+      <div className={styles.centre}>
+        <NewsHeader />
+        <NewsFeed />
+        <NewsFooter />
+      </div>
     </div>
   );
 }
--- src/features/NewsFeed/NewsFeed.tsx
+++ src/features/NewsFeed/NewsFeed.tsx
@@ -1,7 +1,16 @@
 import { FC } from "react";
+import styles from "./NewsFeed.module.css";
 
 const NewsFeed: FC = () => {
-  return <div>News Feed</div>;
+  return (
+    <div className={styles.someNews}>
+      <div>This is the title</div>
+      <div className={styles.flex}>
+        <div className={styles.date}>Date goes here</div>
+        <div className={styles.source}>Source will be here</div>
+      </div>
+    </div>
+  );
 };
 
 export { NewsFeed };
