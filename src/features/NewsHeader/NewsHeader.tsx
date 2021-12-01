c8a2e46f60bfe07a8af71351bc72a3821cc8905a
Luke Phyall <shoreditchwarlock@gmail.com>
2021-12-01 13:01:14 +0000

    Block out NewsHeader

--- src/features/NewsHeader/NewsHeader.tsx
+++ src/features/NewsHeader/NewsHeader.tsx
@@ -1,7 +1,26 @@
 import { FC } from "react";
+import styles from "./NewsHeader.module.css";
 
 const NewsHeader: FC = () => {
-  return <div>News Header</div>;
+  return (
+    <div className={styles.flex}>
+      <h1 className={styles.header}>
+        The News with a totally unnecessary application of Redux
+      </h1>
+      <select
+        className={styles.filter}
+        name={"sourceFilter"}
+        id={"sourceFilter"}
+        defaultValue="filterBySource"
+      >
+        <option value="filterBySource" disabled hidden>
+          Filter By Source
+        </option>
+        <option>The Daily Planet</option>
+        <option>The Gotham Times</option>
+      </select>
+    </div>
+  );
 };
 
 export { NewsHeader };
